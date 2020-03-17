cae78e3139cb7cedfdcfdd649089a5a782f55a6e
Rogério Tavares <rogerioctf@gmail.com>
2020-03-17 09:40:39 -0300

    Estrutura do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+    "import/prefer-default-export": "off",
+    "react/no-unused-state": "warn",
+    "react/state-in-constructor": ["warn", "never"],
+    "react/static-property-placement": "off"
+  }
+};
